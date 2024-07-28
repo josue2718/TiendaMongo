78c9cc171ec1212db6019d8c94040a0bdab88c1b
josue2718 <128634505+josue2718@users.noreply.github.com>
2024-07-27 23:32:59 -0600

    Chupala morfero
    Se va a mojar mofero parte 1 27/07/2024 11:32PM

---
+++ App.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const { MongoClient, ObjectId } = require('mongodb');
+
+const app = express();
+const port = 3002;
+const uris = 'mongodb+srv://mondongoekix:mongo102030@mongocluster0.nucsdpn.mongodb.net/';
+const client = new MongoClient(uris, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use(cors({
+  origin: 'http://127.0.0.1:5500'
+}));
+client.connect()
+  .then(() => {
+    console.log('Conexión establecida con MongoDB.');
+    
+    // Iniciar el servidor
+    app.listen(port, () => {
+      console.log(`Servidor escuchando en http://localhost:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error al conectar con MongoDB:', err);
+    process.exit(1);
+  });
+
+app.delete('/eliminar/:id', async (req, res) => {
+  try {
+    console.log('Conexión establecida con MongoDB.');
+    const db = client.db('tiendas');
+    const collection = db.collection('usuarios');
+    const id = req.params.id;
+
+    const result = await collection.deleteOne({ _id: ObjectId(id) });
+
+    if (result.deletedCount === 1) {
+      console.log(`Documento con ID ${id} eliminado correctamente.`);
+      res.status(200).send(`Documento con ID ${id} eliminado correctamente.`);
+    } else {
+      console.log(`No se encontró documento con ID ${id}.`);
+      res.status(404).send(`No se encontró documento con ID ${id}.`);
+    }
+  } catch (err) {
+    console.error('Error al eliminar documento en MongoDB:', err);
+    res.status(500).send('Error al eliminar documento en MongoDB.');
+  } 
+  
+});
+
+// Ruta para buscar por nombre
+app.get('/usuario', async (req, res) => {
+  try {
+
+    console.log('Conexión establecida con MongoDB.');
+    const db = client.db('tiendas');
+    const collection = db.collection('usuarios');
+    const correo = req.query.correo;
+
+    const result = await collection.find({ correo: correo }).toArray();
+
+    if (result.length > 0) {
+      console.log(`Documentos encontrados con el correo ${correo}:`, result);
+      res.status(200).json(result);
+    } else {
+      console.log(`No se encontraron documentos con el correo ${correo}.`);
+      res.status(404).send(`No se encontraron documentos con el correo ${correo}.`);
+    }
+  } catch (err) {
+    console.error('Error al buscar documento en MongoDB:', err);
+    res.status(500).send('Error al buscar documento en MongoDB.');
+  } 
+  
+});
+
+app.get('/producto', async (req, res) => {
+  try {
+
+    console.log('Conexión establecida con MongoDB.');
+    const db = client.db('tiendas');
+    const collection = db.collection('productos');
+    const correo = req.query.correo;
+
+    const result = await collection.find({}).toArray();
+
+    if (result.length > 0) {
+      console.log(`Documentos encontrados con el correo ${correo}:`, result);
+      res.status(200).json(result);
+    } else {
+      console.log(`No se encontraron documentos con el correo ${correo}.`);
+      res.status(404).send(`No se encontraron documentos con el correo ${correo}.`);
+    }
+  } catch (err) {
+    console.error('Error al buscar documento en MongoDB:', err);
+    res.status(500).send('Error al buscar documento en MongoDB.');
+  } 
+  
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    console.log('login.');
+    const db = client.db('tiendas');
+    const collection = db.collection('usuarios');
+    const { correo, password } = req.body;
+
+    const user = await collection.findOne({ correo: correo });
+    if (correo == user.correo) {
+      const isPasswordCorrect = await bcrypt.compare(password, user.passwordencrypt);
+      if (isPasswordCorrect) {
+        res.status(200).send('Inicio de sesión exitoso.');
+        console.log('Inicio de sesión exitoso.');
+      } else {
+        res.status(401).send('Contraseña incorrecta.');
+        console.log('Contraseña incorrecta.');
+      }
+    } else {
+      res.status(401).send('Correo incorrectos.');
+    }
+  } catch (err) {
+    console.error('Error al buscar usuario en MongoDB:', err);
+    res.status(500).send('Error al buscar usuario en MongoDB.');
+  } 
+});
+
+app.post('/insertarusuarios', async (req, res) => {
+  try {
+    console.log('nuevo usuario');
+    const db = client.db('tiendas');
+    const collection = db.collection('usuarios');
+    const { nombre, correo, password, fechainicio } = req.body;
+
+    const salt = bcrypt.genSaltSync(10);
+    const passwordencrypt = bcrypt.hashSync(password, salt);
+
+    await collection.insertOne({ nombre, correo, passwordencrypt, fechainicio});
+
+    console.log('Datos insertados correctamente en MongoDB.');
+    res.status(200).send('Datos insertados correctamente en MongoDB.');
+  } catch (err) {
+    console.error('Error al insertar datos en MongoDB:', err);
+    res.status(500).send('Error al insertar datos en MongoDB.');
+  }
+});
+
---
+++ index.js
@@ -0,0 +1,75 @@
+document.getElementById('correo').addEventListener('input', async function(event) {
+    event.preventDefault();
+    const correo = document.getElementById('correo').value;
+    const password = document.getElementById('password').value;
+    
+    fetch('http://localhost:3002/usuario?correo=' + encodeURIComponent(correo))
+            .then(res => res.json())
+            .then(res => {
+                res.forEach(item => {
+                    console.log(JSON.stringify(res, null, 2));
+                    console.log(item.correo);
+                }
+            )   
+        })
+    .catch(err => console.error(err));
+})
+document.getElementById('login').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const correo = document.getElementById('correo').value;
+    const password = document.getElementById('password').value;
+    
+    fetch('http://localhost:3002/login', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ correo, password })
+    })
+    .then(res => {
+        if (res.ok) {
+            alert("Bienvenido ");
+            window.location.href = 'ventas.html';
+        return res.text(); 
+        } else {
+        alert('Correo o contraseña incorrectos.');
+        }
+    })
+    .then(responseText => {
+        
+    })
+    .catch(err => console.error(err));
+});
+
+document.getElementById('newuser').addEventListener('submit', async function(event) {
+    event.preventDefault();
+  
+    const correo = document.getElementById('newcorreo').value;
+    const password = document.getElementById('newpassword').value;
+    const nombre = document.getElementById('newnombre').value;
+    const fechainicio = new Date();
+  
+    try {
+      const response = await fetch('http://localhost:3002/insertarusuarios', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nombre, correo, password, fechainicio })
+      });
+  
+      if (response.ok) {
+        alert("Bienvenido " + nombre);
+        location.reload()
+      } else {
+        const errorData = await response.json();
+        console.error('Error al insertar usuario:', errorData);
+        alert('Error al insertar usuario: ' + (errorData.message || response.statusText));
+      }
+    } catch (err) {
+      console.error('Error de red:', err);
+      alert('Error de red: ' + err.message);
+    }
+  });
+  
+      
